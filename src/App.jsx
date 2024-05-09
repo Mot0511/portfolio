04bc5e1cc12e7e9ddac1deac2b2b74651eccc97c
Mot0511 <suvorov.matvej9@gmail.com>
2024-05-09 08:59:59 +0300

    Added global styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import cl from './App.module.css'
+import './global.css'
+
+function App() {
+    return ( 
+        <div className={cl.main}>
+            Русский текст
+        </div>
+    );
+}
+
+export default App;
